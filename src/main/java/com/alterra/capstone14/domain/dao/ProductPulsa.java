e28e125531819f243302d018a02c686c63e7e770
Muhammad Satya Adhi Wicaksana <styadhi.w23@gmail.com>
2022-06-09 15:26:42 +0800

    add ProductPulsa

---
+++ src/main/java/com/alterra/capstone14/domain/dao/ProductPulsa.java
@@ -0,0 +1,29 @@
+package com.alterra.capstone14.domain.dao;
+
+
+import javax.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@Table(name= "product_pulsa")
+@Entity
+public class ProductPulsa {
+    @Id
+    private Long id;
+
+    @Column(name = "pulsa_id")
+    private Long pulsaId;
+
+    @Column(name = "denom")
+    private int denom;
+
+    @Column(name = "price")
+    private int price;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "stock")
+    private int stock;
+}
